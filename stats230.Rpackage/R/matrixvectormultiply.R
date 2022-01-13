7da5cd3220d073ccd994acc9d3a294fd93ec870c
John Corrette <jcorrett@uci.edu>
2022-01-12 21:17:05 -0800

    Added matrixvectormultiply function and documented

---
+++ stats230.Rpackage/R/matrixvectormultiply.R
@@ -0,0 +1,16 @@
+#' Compute matrix-vector product ABx
+#'
+#' @param A A matrix
+#' @param B A matrix
+#' @param x A column vector
+#' @param switch_var A boolean
+matrixvectormultiply <- function(A, B, x, switch_var) {
+  if (switch_var == TRUE) {
+    prod <- A%*%B
+    prod <- prod%*%x
+  } else {
+    prod <- B%*%x
+    prod <- A%*%prod
+  }
+  return(prod)
+}
