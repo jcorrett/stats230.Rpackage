4b24c668b9e068323010c4cad50caf6017bfb3fa
John Corrette <jcorrett@uci.edu>
2022-01-14 13:59:16 -0800

    Modified description to be more clear

--- R/matrixvectormultiply.R
+++ R/matrixvectormultiply.R
@@ -3,7 +3,7 @@
 #' @param A A matrix
 #' @param B A matrix
 #' @param x A column vector
-#' @param switch_var A boolean
+#' @param switch_var A boolean value to switch algorithms. TRUE computes (AB)x, whereas FALSE computes A(Bx).
 #'
 #' @export
 matrixvectormultiply <- function(A, B, x, switch_var) {
