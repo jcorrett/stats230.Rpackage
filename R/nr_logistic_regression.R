2571cdbd3f3ac57c749ed40dfbbe1548f28468ce
John Corrette <jcorrett@uci.edu>
2022-02-27 18:29:45 -0800

    added nr_logistic_regression function

---
+++ R/nr_logistic_regression.R
@@ -0,0 +1,26 @@
+#' Solve logistic regression problem via Newton-Raphson algorithm
+#'
+#' @param X A n by d matrix
+#' @param y A n by 1 column vector
+#' @param alpha A non-negative error tolerance
+#'
+#' @export
+nr_logistic_regression <- function(X,y,tol) {
+  llhood <- c()
+  d <- dim(X)
+  beta <- as.matrix(rnorm(d[2]))
+  p <- 1/(1+exp(-X%*%beta))
+  err <- Inf #To ensure we enter the loop
+  while(err > tol) {
+    llhood <- c(llhood,t(y) %*% X %*% beta -sum(log(1+exp(X%*%beta))))
+    gradllhood <- t(X)%*%(1/(1+exp(-X%*%beta)) - y)
+    M <- inv(t(X)%*%diag(as.vector(p*(1-p)))%*%X)
+    beta <- beta + M%*%gradllhood
+    p <- 1/(1+exp(-X%*%beta))
+    err <- norm(M%*%gradllhood)
+  }
+  p <- 1/(1+exp(-X%*%beta))
+  W <- diag(as.vector(p*(1-p)))
+  CI_bounds <- 1.960*sqrt(diag(inv(t(X)%*%W%*%X)))
+  return(list(beta,llhood,CI_bounds))
+}
